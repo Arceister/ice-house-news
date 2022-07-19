2369bc4ca976069d08b0627caa690734d469f90c
arceister <imandajagad01@gmail.com>
2022-07-19 13:52:48 +0700

    Refactor(News Entity): remove pointer not nil attributes struct

--- entity/news.go
+++ entity/news.go
@@ -8,10 +8,10 @@ import (
 
 type NewsListOutput struct {
 	Id               uuid.UUID    `json:"id"`
-	Title            *string      `json:"title"`
-	SlugUrl          *string      `json:"slug_url"`
+	Title            string       `json:"title"`
+	SlugUrl          string       `json:"slug_url"`
 	CoverImage       *string      `json:"cover_image"`
-	AdditionalImages []*string    `json:"additional_images"`
+	AdditionalImages []string     `json:"additional_images"`
 	CreatedAt        time.Time    `json:"created_at"`
 	Nsfw             bool         `json:"nsfw"`
 	Category         NewsCategory `json:"category"`
@@ -20,12 +20,12 @@ type NewsListOutput struct {
 
 type NewsCategory struct {
 	Id   uuid.UUID `json:"id"`
-	Name *string   `json:"name"`
+	Name string    `json:"name"`
 }
 
 type NewsCounter struct {
-	Upvote   *int32 `json:"upvote"`
-	Downvote *int32 `json:"downvote"`
-	Comment  *int32 `json:"comment"`
-	View     *int32 `json:"view"`
+	Upvote   int32 `json:"upvote"`
+	Downvote int32 `json:"downvote"`
+	Comment  int32 `json:"comment"`
+	View     int32 `json:"view"`
 }
