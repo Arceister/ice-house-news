eabcbab89e363400c269baa7e75037278ff9e8c1
arceister <imandajagad01@gmail.com>
2022-07-26 18:47:09 +0700

    Test (User Repository): add `createUser` OK `testcase`

--- repository/users_test.go
+++ repository/users_test.go
@@ -24,17 +24,17 @@ func TestGetOneUserRepository(t *testing.T) {
 	)
 
 	tests := []struct {
-		name    string
-		s       UsersRepository
-		msgUUID string
-		mock    func()
-		want    entity.User
-		wantErr bool
+		name     string
+		s        UsersRepository
+		userUUID string
+		mock     func()
+		want     entity.User
+		wantErr  bool
 	}{
 		{
-			name:    "OK",
-			s:       app,
-			msgUUID: "72908c48-b68c-4d67-ae74-d1305f84fc4d",
+			name:     "OK",
+			s:        app,
+			userUUID: "72908c48-b68c-4d67-ae74-d1305f84fc4d",
 			mock: func() {
 				rows := sqlmock.NewRows(
 					[]string{"id", "email", "password", "name", "bio", "web", "picture"},
@@ -55,9 +55,9 @@ func TestGetOneUserRepository(t *testing.T) {
 			wantErr: false,
 		},
 		{
-			name:    "Not Found",
-			s:       app,
-			msgUUID: "72908c48-b68c-4d67-ae74-d1305f84fc4d",
+			name:     "Not Found",
+			s:        app,
+			userUUID: "72908c48-b68c-4d67-ae74-d1305f84fc4d",
 			mock: func() {
 				rows := sqlmock.NewRows(
 					[]string{"id", "email", "password", "name", "bio", "web", "picture"},
@@ -70,7 +70,7 @@ func TestGetOneUserRepository(t *testing.T) {
 	for _, tt := range tests {
 		t.Run(tt.name, func(t *testing.T) {
 			tt.mock()
-			got, err := tt.s.GetOneUserRepository(tt.msgUUID)
+			got, err := tt.s.GetOneUserRepository(tt.userUUID)
 			if (err != nil) != tt.wantErr {
 				t.Errorf("Get() error new = %v, wantErr %v", err, tt.wantErr)
 				return
@@ -78,6 +78,58 @@ func TestGetOneUserRepository(t *testing.T) {
 
 			if err != nil && !reflect.DeepEqual(got, tt.want) {
 				t.Errorf("Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
+func TestCreateUserRepository(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	app := NewUsersRepository(
+		lib.DB{
+			DB: db,
+		},
+	)
+	tests := []struct {
+		name     string
+		s        UsersRepository
+		userUUID uuid.UUID
+		request  entity.User
+		mock     func()
+		wantErr  bool
+	}{
+		{
+			name:     "OK",
+			s:        app,
+			userUUID: uuid.MustParse("72908c48-b68c-4d67-ae74-d1305f84fc4d"),
+			request: entity.User{
+				Email:    "testemail@email.com",
+				Password: "123",
+				Name:     "Jagad",
+				Bio:      func(val string) *string { return &val }("Bio"),
+				Web:      func(val string) *string { return &val }("Web"),
+				Picture:  func(val string) *string { return &val }("Picture"),
+			},
+			mock: func() {
+				mock.ExpectExec("INSERT INTO users").
+					WithArgs("72908c48-b68c-4d67-ae74-d1305f84fc4d", "testemail@email.com", "123", "Jagad", "Bio", "Web", "Picture").
+					WillReturnResult(sqlmock.NewResult(1, 1))
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
+			err := tt.s.CreateUserRepository(tt.userUUID, tt.request)
+			if (err != nil) != tt.wantErr {
+				t.Error(err)
+				return
 			}
 		})
 	}
