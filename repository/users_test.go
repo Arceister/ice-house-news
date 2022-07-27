d071d2b49809d658a24443112fb067e01be139f0
arceister <imandajagad01@gmail.com>
2022-07-27 11:24:32 +0700

    Test (User Repository): add `getUserByEmail` OK `testcase`

--- repository/users_test.go
+++ repository/users_test.go
@@ -84,6 +84,67 @@ func TestGetOneUserRepository(t *testing.T) {
 	}
 }
 
+func TestGetUserByEmailRepository(t *testing.T) {
+	db, mock, err := sqlmock.New()
+	if err != nil {
+		t.Fatal(err)
+	}
+	defer db.Close()
+
+	app := NewUsersRepository(
+		lib.DB{
+			DB: db,
+		},
+	)
+
+	tests := []struct {
+		name      string
+		s         UsersRepository
+		userEmail string
+		mock      func()
+		want      entity.User
+		wantErr   bool
+	}{
+		{
+			name:      "OK",
+			s:         app,
+			userEmail: "testemail@email.com",
+			mock: func() {
+				rows := sqlmock.NewRows(
+					[]string{"id", "email", "password", "name", "bio", "web", "picture"},
+				).
+					AddRow("72908c48-b68c-4d67-ae74-d1305f84fc4d", "testemail@email.com", "123", "Jagad", "Bio", "Web", "Picture")
+
+				mock.ExpectQuery("SELECT (.+) FROM users").WithArgs("testemail@email.com").WillReturnRows(rows)
+			},
+			want: entity.User{
+				Id:       uuid.MustParse("72908c48-b68c-4d67-ae74-d1305f84fc4d"),
+				Email:    "testemail@email.com",
+				Password: "123",
+				Name:     "Name",
+				Bio:      func(val string) *string { return &val }("Bio"),
+				Web:      func(val string) *string { return &val }("Web"),
+				Picture:  func(val string) *string { return &val }("Picture"),
+			},
+			wantErr: false,
+		},
+	}
+	for _, tt := range tests {
+		t.Run(tt.name, func(t *testing.T) {
+			tt.mock()
+			got, err := tt.s.GetUserByEmailRepository(tt.userEmail)
+			if (err != nil) != tt.wantErr {
+				t.Errorf("Get() error new = %v, wantErr %v", err, tt.wantErr)
+				return
+			}
+
+			if err != nil && !reflect.DeepEqual(got, tt.want) {
+				t.Errorf("Get() = %v, want %v", got, tt.want)
+			}
+		})
+	}
+}
+
 func TestCreateUserRepository(t *testing.T) {
 	db, mock, err := sqlmock.New()
 	if err != nil {
