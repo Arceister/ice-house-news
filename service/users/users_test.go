040d03370bc085104e0ccd8a6d4b9ef3464f7c73
arceister <imandajagad01@gmail.com>
2022-08-02 23:05:58 +0700

    Test(User Service): add `success` testcase on `extendToken`

--- service/users/users_test.go
+++ service/users/users_test.go
@@ -250,3 +250,31 @@ func TestUsersService_SignIn_InternalServerError(t *testing.T) {
 	assert.EqualValues(t, err.Status(), http.StatusInternalServerError)
 	assert.EqualValues(t, err.Message(), "error message")
 }
+
+func TestUsersService_ExtendToken_Success(t *testing.T) {
+	mockRepository := NewRepositoryMock()
+	middleware := middleware.NewMiddlewareJWT(lib.App{Port: ":5000", SecretKey: "SECRET"})
+
+	getOneUser = func(id string) (entity.User, errorUtils.IErrorMessage) {
+		return entity.User{
+			Id:       uuid.MustParse("8db82f7e-5736-4430-a62c-2e735177d895"),
+			Email:    "testemail@email.com",
+			Password: "123",
+			Name:     "Jagad",
+			Bio:      func(val string) *string { return &val }("Bio"),
+			Web:      func(val string) *string { return &val }("Web"),
+			Picture:  func(val string) *string { return &val }("Picture"),
+		}, nil
+	}
+
+	mockService := NewUsersService(mockRepository, middleware)
+	userAuthenticationReturn, err := mockService.ExtendToken("8db82f7e-5736-4430-a62c-2e735177d895")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotNil(t, userAuthenticationReturn)
+	assert.Nil(t, err)
+	assert.EqualValues(t, userAuthenticationReturn.Scheme, "Bearer")
+	assert.NotNil(t, userAuthenticationReturn)
+}
