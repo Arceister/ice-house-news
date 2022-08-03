9623cb92348a84a63dbc5c4b96855dfd699f09bd
arceister <imandajagad01@gmail.com>
2022-08-03 23:40:01 +0700

    Test(User Service): add `success` testcase on `GetProfile`

--- handler/users/users_test.go
+++ handler/users/users_test.go
@@ -1,14 +1,18 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
 	"testing"
 
 	"github.com/Arceister/ice-house-news/entity"
+	"github.com/Arceister/ice-house-news/lib"
+	"github.com/Arceister/ice-house-news/middleware"
 	"github.com/Arceister/ice-house-news/service"
 	errorUtils "github.com/Arceister/ice-house-news/utils/error"
+	"github.com/golang-jwt/jwt/v4"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
@@ -144,4 +148,88 @@ func TestUserHandler_GetOneUser_Error(t *testing.T) {
 	assert.EqualValues(t, http.StatusInternalServerError, w.Code)
 	assert.EqualValues(t, false, httpResponse.Success)
 	assert.EqualValues(t, "error message", httpResponse.Message)
+}
+
+func TestUserHandler_GetOwnProfile_Success(t *testing.T) {
+	mockService := NewServiceMock()
+
+	type ResponseDataStruct struct {
+		entity.User
+	}
+
+	type successStruct struct {
+		Success bool               `json:"success"`
+		Message string             `json:"message"`
+		Data    ResponseDataStruct `json:"data"`
+	}
+
+	getOneUser = func(s string) (entity.User, errorUtils.IErrorMessage) {
+		return entity.User{
+			Id:       uuid.MustParse("8db82f7e-5736-4430-a62c-2e735177d895"),
+			Email:    "testemail@email.com",
+			Password: "123",
+			Name:     "Jagad",
+			Bio:      func(val string) *string { return &val }("Bio"),
+			Web:      func(val string) *string { return &val }("Web"),
+			Picture:  func(val string) *string { return &val }("Picture"),
+		}, nil
+	}
+
+	userId := "8db82f7e-5736-4430-a62c-2e735177d895"
+
+	mockHandler := NewUsersHandler(mockService)
+	middlewareMock := middleware.NewMiddlewareJWT(lib.App{Port: ":5000", SecretKey: "SECRET"})
+
+	fakeTokenString, _, err := middlewareMock.GenerateNewToken(
+		entity.User{
+			Id:       uuid.MustParse("8db82f7e-5736-4430-a62c-2e735177d895"),
+			Email:    "testemail@email.com",
+			Password: "123",
+			Name:     "Jagad",
+			Bio:      func(val string) *string { return &val }("Bio"),
+			Web:      func(val string) *string { return &val }("Web"),
+			Picture:  func(val string) *string { return &val }("Picture"),
+		},
+	)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fakeToken, err := jwt.Parse(*fakeTokenString, func(t *jwt.Token) (interface{}, error) {
+		return []byte("SECRET"), nil
+	})
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	fakeClaims, _ := fakeToken.Claims.(jwt.MapClaims)
+
+	req, err := http.NewRequest("GET", "http://localhost:5055/api/users/"+userId, nil)
+	req = req.WithContext(context.WithValue(req.Context(), "JWTProps", fakeClaims))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	mockHandler.GetOwnProfile(w, req)
+
+	var httpResponse successStruct
+	err = json.Unmarshal([]byte(w.Body.Bytes()), &httpResponse)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotNil(t, httpResponse)
+	assert.Nil(t, err)
+	assert.EqualValues(t, http.StatusOK, w.Code)
+	assert.EqualValues(t, true, httpResponse.Success)
+	assert.EqualValues(t, "get profile", httpResponse.Message)
+	assert.EqualValues(t, uuid.MustParse(userId), httpResponse.Data.Id)
+	assert.EqualValues(t, "testemail@email.com", httpResponse.Data.Email)
+	assert.EqualValues(t, "123", httpResponse.Data.Password)
+	assert.EqualValues(t, "Jagad", httpResponse.Data.Name)
+	assert.EqualValues(t, func(val string) *string { return &val }("Bio"), httpResponse.Data.Bio)
+	assert.EqualValues(t, func(val string) *string { return &val }("Web"), httpResponse.Data.Web)
+	assert.EqualValues(t, func(val string) *string { return &val }("Picture"), httpResponse.Data.Picture)
 }
