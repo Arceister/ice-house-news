f1a2e6e8bfa6bf64c1c44641e9fcf6145caec1fb
arceister <imandajagad01@gmail.com>
2022-08-04 12:25:45 +0700

    Test(User Service): add `error` testcase on `UpdateUser`

--- handler/users/users_test.go
+++ handler/users/users_test.go
@@ -429,3 +429,44 @@ func TestUserHandler_UpdateUser_Success(t *testing.T) {
 	assert.EqualValues(t, true, httpResponse.Success)
 	assert.EqualValues(t, "update user success", httpResponse.Message)
 }
+
+func TestUserHandler_Update_Error(t *testing.T) {
+	mockService := NewServiceMock()
+
+	type errorStruct struct {
+		Success bool   `json:"success"`
+		Message string `json:"message"`
+	}
+
+	updateUser = func(s string, u entity.User) errorUtils.IErrorMessage {
+		return errorUtils.NewInternalServerError("error message")
+	}
+
+	mockHandler := NewUsersHandler(mockService)
+
+	userId := "8db82f7e-5736-4430-a62c-2e735177d895"
+	jsonRequest := `{
+		""
+	}`
+
+	req, err := http.NewRequest("PUT", "http://localhost:5055/api/users/"+userId, bytes.NewBufferString(jsonRequest))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	w := httptest.NewRecorder()
+
+	mockHandler.UpdateUserHandler(w, req)
+
+	var httpResponse errorStruct
+	err = json.Unmarshal([]byte(w.Body.Bytes()), &httpResponse)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	assert.NotNil(t, httpResponse)
+	assert.Nil(t, err)
+	assert.EqualValues(t, http.StatusInternalServerError, w.Code)
+	assert.EqualValues(t, false, httpResponse.Success)
+	assert.EqualValues(t, "error message", httpResponse.Message)
+}
