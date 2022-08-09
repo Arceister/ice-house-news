bea8a1f1a9e9e02789c649763af93d4583bd81df
arceister <imandajagad01@gmail.com>
2022-08-09 13:18:40 +0700

    Refactor: add validation to comment insert

--- service/comment/comment.go
+++ service/comment/comment.go
@@ -3,6 +3,7 @@ package service
 import (
 	"github.com/Arceister/ice-house-news/entity"
 	"github.com/Arceister/ice-house-news/service"
+	"github.com/go-playground/validator/v10"
 	"github.com/google/uuid"
 
 	commentRepository "github.com/Arceister/ice-house-news/repository/comment"
@@ -42,6 +43,13 @@ func (s *CommentService) InsertCommentService(
 	userId string,
 ) errorUtils.IErrorMessage {
 	var commentInsert entity.CommentInsert
+
+	validate := validator.New()
+	validationError := validate.Struct(commentRequest)
+
+	if validationError != nil {
+		return errorUtils.NewUnprocessableEntityError("insert invalid")
+	}
 
 	newCommentUUID := uuid.Must(uuid.NewRandom())
 	newsUUID := uuid.MustParse(newsId)
