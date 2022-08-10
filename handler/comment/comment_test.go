347ed4f5198d3fdc40ff623887e95f2d6a8b8b3d
arceister <imandajagad01@gmail.com>
2022-08-10 17:58:18 +0700

    Test(Comment Handler): add `GetComments` testcase with result `Error`

--- handler/comment/comment_test.go
+++ handler/comment/comment_test.go
@@ -11,6 +11,7 @@ import (
 
 	"github.com/Arceister/ice-house-news/entity"
 	serviceMock "github.com/Arceister/ice-house-news/service/mock"
+	errorUtils "github.com/Arceister/ice-house-news/utils/error"
 	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
@@ -68,6 +69,36 @@ func TestCommentHandler_GetComments(t *testing.T) {
 		assert.EqualValues(t, true, httpResponse.Success)
 		assert.EqualValues(t, "comments get", httpResponse.Message)
 		assert.EqualValues(t, mockAllComment, httpResponse.Data)
+
+		mockCommentService.AssertExpectations(t)
+	})
+
+	t.Run("Error", func(t *testing.T) {
+		type errorStruct struct {
+			Success bool   `json:"success"`
+			Message string `json:"message"`
+		}
+
+		w := httptest.NewRecorder()
+		routerCtx := chi.NewRouteContext()
+		routerCtx.URLParams.Add("newsId", newsId)
+		req = req.WithContext(context.WithValue(context.Background(), chi.RouteCtxKey, routerCtx))
+
+		mockCommentService.On("GetCommentsOnNewsService", newsId).
+			Return(nil, errorUtils.NewInternalServerError("error message")).Once()
+
+		mockHandler.GetCommentsOnNewsHandler(w, req)
+
+		var httpResponse errorStruct
+		err := json.Unmarshal([]byte(w.Body.Bytes()), &httpResponse)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.Nil(t, err)
+		assert.EqualValues(t, http.StatusInternalServerError, w.Code)
+		assert.EqualValues(t, false, httpResponse.Success)
+		assert.EqualValues(t, "error message", httpResponse.Message)
 
 		mockCommentService.AssertExpectations(t)
 	})
