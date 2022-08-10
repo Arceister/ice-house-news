4663cee0408a76185a8ae54bf95214b38839b79e
arceister <imandajagad01@gmail.com>
2022-08-11 02:49:53 +0700

    Test(News Handler): add `GetNewsDetail` testcase with result `Success`

--- handler/news/news_test.go
+++ handler/news/news_test.go
@@ -1,6 +1,7 @@
 package handler
 
 import (
+	"context"
 	"encoding/json"
 	"net/http"
 	"net/http/httptest"
@@ -10,6 +11,7 @@ import (
 	"github.com/Arceister/ice-house-news/entity"
 	serviceMock "github.com/Arceister/ice-house-news/service/mock"
 	errorUtils "github.com/Arceister/ice-house-news/utils/error"
+	"github.com/go-chi/chi/v5"
 	"github.com/google/uuid"
 	"github.com/stretchr/testify/assert"
 )
@@ -114,6 +116,77 @@ func TestNewsHandler_GetNewsList(t *testing.T) {
 		assert.EqualValues(t, http.StatusInternalServerError, w.Code)
 		assert.EqualValues(t, false, httpResponse.Success)
 		assert.EqualValues(t, "error message", httpResponse.Message)
+
+		mockNewsService.AssertExpectations(t)
+	})
+}
+
+func TestNewsHandler_GetNewsDetail(t *testing.T) {
+	mockNewsService := new(serviceMock.NewsServiceMock)
+
+	mockNewsResult := entity.NewsDetail{
+		Id:               uuid.MustParse("922c7afd-643e-4e44-ab51-c80dc137674a"),
+		Title:            "News Title",
+		SlugUrl:          "news-title",
+		CoverImage:       func(val string) *string { return &val }("Cover"),
+		AdditionalImages: []string{"ABC"},
+		CreatedAt:        time.Time{},
+		Nsfw:             false,
+		Category: entity.NewsCategory{
+			Id:   uuid.MustParse("d414197c-0fa0-46c1-ac29-69c4cdc0ed11"),
+			Name: "Howak",
+		},
+		Author: entity.NewsAuthor{
+			Id:      uuid.MustParse("e65d7793-bcc6-467c-88b1-9636ee745f45"),
+			Name:    "Name",
+			Picture: func(val string) *string { return &val }("Picture"),
+		},
+		Counter: entity.NewsCounter{
+			Upvote:   10,
+			Downvote: 23,
+			Comment:  2,
+			View:     1000,
+		},
+		Content: "Lorem Ipsum",
+	}
+
+	mockHandler := NewNewsHandler(mockNewsService)
+	newsId := "922c7afd-643e-4e44-ab51-c80dc137674a"
+
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:5055/api/news"+newsId, nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Success", func(t *testing.T) {
+		type successStruct struct {
+			Success bool              `json:"success"`
+			Message string            `json:"message"`
+			Data    entity.NewsDetail `json:"data"`
+		}
+
+		w := httptest.NewRecorder()
+		routerCtx := chi.NewRouteContext()
+		routerCtx.URLParams.Add("newsId", newsId)
+		req = req.WithContext(context.WithValue(context.Background(), chi.RouteCtxKey, routerCtx))
+
+		mockNewsService.On("GetNewsDetailService", newsId).
+			Return(mockNewsResult, nil).Once()
+
+		mockHandler.GetNewsDetailHandler(w, req)
+
+		var httpResponse successStruct
+		err := json.Unmarshal([]byte(w.Body.Bytes()), &httpResponse)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.NotNil(t, httpResponse)
+		assert.Nil(t, err)
+		assert.EqualValues(t, http.StatusOK, w.Code)
+		assert.EqualValues(t, true, httpResponse.Success)
+		assert.EqualValues(t, "get news detail", httpResponse.Message)
+		assert.EqualValues(t, mockNewsResult, httpResponse.Data)
 
 		mockNewsService.AssertExpectations(t)
 	})
