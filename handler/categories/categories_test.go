cfed3e5845b600a2f3f2798d142ad23d0d49071d
arceister <imandajagad01@gmail.com>
2022-08-10 23:49:47 +0700

    Test(Categories Handler): add `GetAllCategories` testcase with result `Success`

---
+++ handler/categories/categories_test.go
@@ -0,0 +1,60 @@
+package handler
+
+import (
+	"encoding/json"
+	"net/http"
+	"net/http/httptest"
+	"testing"
+
+	"github.com/Arceister/ice-house-news/entity"
+	serviceMock "github.com/Arceister/ice-house-news/service/mock"
+	"github.com/google/uuid"
+	"github.com/stretchr/testify/assert"
+)
+
+func TestCategoriesHandler_GetNewsCategories(t *testing.T) {
+	mockCategoriesService := new(serviceMock.CategoriesServiceMock)
+
+	mockCategory := entity.Categories{
+		Id:   uuid.MustParse("00ac3c8f-62b1-4d80-8c67-b448423048e9"),
+		Name: "Howak",
+	}
+	mockCategories := []entity.Categories{mockCategory}
+
+	mockHandler := NewCategoriesHandler(mockCategoriesService)
+
+	req, err := http.NewRequest(http.MethodGet, "http://localhost:5055/api/news/category", nil)
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	t.Run("Success", func(t *testing.T) {
+		type successStruct struct {
+			Success bool                `json:"success"`
+			Message string              `json:"message"`
+			Data    []entity.Categories `json:"data"`
+		}
+
+		w := httptest.NewRecorder()
+
+		mockCategoriesService.On("GetAllNewsCategoryService").
+			Return(mockCategories, nil).Once()
+
+		mockHandler.GetAllNewsCategoryHandler(w, req)
+
+		var httpResponse successStruct
+		err := json.Unmarshal([]byte(w.Body.Bytes()), &httpResponse)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.NotNil(t, httpResponse)
+		assert.Nil(t, err)
+		assert.EqualValues(t, http.StatusOK, w.Code)
+		assert.EqualValues(t, true, httpResponse.Success)
+		assert.EqualValues(t, "get all categories", httpResponse.Message)
+		assert.EqualValues(t, mockCategories, httpResponse.Data)
+
+		mockCategoriesService.AssertExpectations(t)
+	})
+}
