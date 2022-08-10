99380619a4556fab32a3476d39416aef94854b62
arceister <imandajagad01@gmail.com>
2022-08-11 01:57:17 +0700

    Test(News Service): add `GetNewsDetail` testcase with result `Success`

--- service/news/news_test.go
+++ service/news/news_test.go
@@ -91,3 +91,55 @@ func TestNewsService_GetNewsList(t *testing.T) {
 		mockNewsRepo.AssertExpectations(t)
 	})
 }
+
+func TestNewsService_GetNewsDetail(t *testing.T) {
+	mockNewsRepo := new(repositoryMock.NewsRepositoryMock)
+	mockUsersRepo := new(repositoryMock.UserRepositoryMock)
+	mockCategoriesRepo := new(repositoryMock.CategoriesRepositoryMock)
+
+	mockNewsResult := entity.NewsDetail{
+		Id:               uuid.MustParse("922c7afd-643e-4e44-ab51-c80dc137674a"),
+		Title:            "News Title",
+		SlugUrl:          "news-title",
+		CoverImage:       func(val string) *string { return &val }("Cover"),
+		AdditionalImages: []string{"ABC"},
+		CreatedAt:        time.Time{},
+		Nsfw:             false,
+		Category: entity.NewsCategory{
+			Id:   uuid.MustParse("d414197c-0fa0-46c1-ac29-69c4cdc0ed11"),
+			Name: "Howak",
+		},
+		Author: entity.NewsAuthor{
+			Id:      uuid.MustParse("e65d7793-bcc6-467c-88b1-9636ee745f45"),
+			Name:    "Name",
+			Picture: func(val string) *string { return &val }("Picture"),
+		},
+		Counter: entity.NewsCounter{
+			Upvote:   10,
+			Downvote: 23,
+			Comment:  2,
+			View:     1000,
+		},
+		Content: "Lorem Ipsum",
+	}
+
+	mockNewsService := NewNewsService(mockNewsRepo, mockUsersRepo, mockCategoriesRepo)
+
+	newsId := "922c7afd-643e-4e44-ab51-c80dc137674a"
+
+	t.Run("Success", func(t *testing.T) {
+		mockNewsRepo.On("GetNewsDetailRepository", newsId).
+			Return(mockNewsResult, nil).Once()
+
+		news, err := mockNewsService.GetNewsDetailService(newsId)
+		if err != nil {
+			t.Fatal(err)
+		}
+
+		assert.NotNil(t, news)
+		assert.Nil(t, err)
+		assert.EqualValues(t, mockNewsResult, news)
+
+		mockNewsRepo.AssertExpectations(t)
+	})
+}
